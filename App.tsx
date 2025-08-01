58f3064027b8fe900fb8acb048612e2014062078
yashhadade <yashhadade13@gmail.com>
2025-08-01 22:37:20 +0530

    Flat Cards

--- App.tsx
+++ App.tsx
@@ -1,28 +1,28 @@
-import React, { JSX, useState } from "react";
-import { View, Text, SafeAreaView, StyleSheet,TouchableOpacity,useColorScheme} from "react-native";
+import React, { JSX, useState } from 'react';
+import {
+  View,
+  Text,
+  // SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  useColorScheme,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  ScrollView,
+} from 'react-native';
+import FlatCards from './components/FlatCards';
 
-function App():JSX.Element {
-  const [isDarkMode,setIsDarMode]=useState(useColorScheme()==="dark")
+function App(): JSX.Element {
+  const paddingTop = Platform.OS === 'android' ? StatusBar.currentHeight || 0 : 0;
+  const [isDarkMode, setIsDarMode] = useState(useColorScheme() === 'dark');
   return (
-    <View style={style.constiner}>
-<Text style={isDarkMode?style.whiteText:style.blackText}> Hello world </Text>
-    </View>
+    <SafeAreaView style={[{paddingTop}]}>
+      <ScrollView>
+      <FlatCards/>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const style= StyleSheet.create({
-  constiner:{
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center",
-    // backgroundColor:"#f5f5f5"
-  },
-  whiteText:{
-    color:"#f5f5f5"
-  },
-  blackText:{
-    color:"#070707ff"
-  }
-})
 
 export default App;
---
+++ components/FlatCards.tsx
@@ -0,0 +1,57 @@
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+
+const FlatCards = () => {
+  return (
+    <View style={style.FlatCards}>
+      <Text style={style.headingText}>FlatCards</Text>
+      <View style={style.threemainCard}>
+        <View style={[style.card, style.cardOne]}>
+          <Text>Red</Text>
+        </View>
+        <View style={[style.card, style.cardTwo]}>
+          <Text>green</Text>
+        </View>
+        <View style={[style.card, style.cardThree]}>
+          <Text>Yellow</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+const style = StyleSheet.create({
+  headingText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#5d5858ff',
+    paddingHorizontal: 8,
+  },
+  FlatCards: {
+    padding: 10,
+    backgroundColor: '#f7a7a7ff',
+  },
+  threemainCard: {
+    // flex:1,
+    flexDirection: 'row',
+    // gap:3
+    justifyContent: 'space-around',
+  },
+  card: {
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems:"center",
+    fontSize:25,
+    borderRadius:6
+  },
+  cardOne: {
+    backgroundColor:"#eb3232ff"
+  },
+  cardTwo: {
+    backgroundColor:"#4edd31ff"
+  },
+  cardThree: {
+    backgroundColor:"#ebd932ff"
+  },
+});
+export default FlatCards;
